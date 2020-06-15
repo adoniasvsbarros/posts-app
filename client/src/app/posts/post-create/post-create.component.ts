b931ca3a058bfea1ee905566079934782cc40fac
adoniasvsbarros <adoniasvicente@hotmail.com>
2020-06-15 11:43:18 -0400

    add posts to postList with event emitter, input and output

--- client/src/app/posts/post-create/post-create.component.ts
+++ client/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -6,13 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  enteredValue = ''
-  newPost = 'NO CONTENT'
+  enteredTitle = '';
+  enteredContent = '';
+
+  @Output() postCreated: EventEmitter<{title: string, content: string}> = new EventEmitter();
 
   constructor(){}
 
   onAddPost(): void{
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+
+    this.postCreated.emit(post);
   }
 
 }
