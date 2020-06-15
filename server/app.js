777dc576c0ffa7a512b56964823b77d618e5eac8
adoniasvsbarros <adoniasvicente@hotmail.com>
2020-06-15 18:12:27 -0400

    adding basic get posts endpoint

--- server/app.js
+++ server/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('first middleware');
-    next();
-});
+app.use("/api/posts", (req, res, next) => {
+    const posts = [
+        {id: "1kjh321h", title: "first title", content:"first commes from server"},
+        {id: "1dd3411c", title: "second title", content:"second commes from server"},
+        {id: "abcuf123", title: "third title", content:"third commes from server"}
+    ];
 
-app.use((req, res, next) => {
-    res.send('hello from express');
+    res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: posts
+    });
 });
 
 module.exports = app;
