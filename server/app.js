c32a86fcedd6bdf00e3e34399718c87f3383e32b
adoniasvsbarros <adoniasvicente@hotmail.com>
2020-06-16 17:58:39 -0400

    deleting posts front and back

--- server/app.js
+++ server/app.js
@@ -22,13 +22,14 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers", 
-        "Origin, X-Request-With, Content-Type, Accept",
-        "Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, PUT, OPTIONS"
+        "Origin, X-Request-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS"
     );
     next();
 });
 
-// POST - Posts
 app.post("/api/posts", (req, res, next) => {
     const post = new Post({
         title: req.body.title,
@@ -40,7 +41,6 @@ app.post("/api/posts", (req, res, next) => {
     });
 });
 
-// GET - Posts
 app.get("/api/posts", (req, res, next) => {
     Post.find().then(documents => {
         res.status(200).json({
@@ -48,6 +48,13 @@ app.get("/api/posts", (req, res, next) => {
             posts: documents
         });
     });    
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+    Post.deleteOne({_id: req.params.id}).then(result => {
+        console.log(result);
+        res.status(200).json({message: 'post deleted!'});
+    });
 });
 
 module.exports = app;
