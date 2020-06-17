b4e44f8b8fe3a6012d8fc842320f51a8e9a7ea5b
adoniasvsbarros <adoniasvicente@hotmail.com>
2020-06-17 19:18:54 -0400

    check auth middleware

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const multer = require('multer');
 
+const Post = require('../models/post');
+const checkAuth = require('../middlewares/check-auth');
+
 const router = express.Router();
 
 const MIME_TYPE_MAP = {
@@ -24,8 +27,6 @@ const storage = multer.diskStorage({
         cb(null, name + '-' + Date.now() + '.' + ext);
     }
 });
-
-const Post = require('../models/post');
 
 router.get("", (req, res, next) => {
     const pageSize = +req.query.pageSize;
@@ -66,7 +67,7 @@ router.get("/:id", (req, res, next) => {
     });
 });
 
-router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
+router.post("", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
     const url = req.protocol + '://' + req.get("host"); 
 
     const post = new Post({
@@ -87,7 +88,7 @@ router.post("", multer({storage: storage}).single("image"), (req, res, next) => 
     
 });
 
-router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+router.put("/:id", checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
     let imagePath = req.body.imagePath;
     if(req.file){
         const url = req.protocol + '://' + req.get("host"); 
@@ -108,7 +109,7 @@ router.put("/:id", multer({storage: storage}).single("image"), (req, res, next) 
     });
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
     Post.deleteOne({_id: req.params.id}).then(result => {
         res.status(200).json({message: 'post deleted!'});
     });
