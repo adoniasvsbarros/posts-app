f8db732aa1709b811fc7a98bf8eac203c757e032
developer <adoniasvicente@hotmail.com>
2020-06-19 13:38:42 -0400

    improve UI header according to auth state

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from '../models/auth.model';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient){}
 
   getToken(): string {
     return this.token;
+  }
+
+  getAuthStatusListener(): Observable<boolean>{
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string){
@@ -27,6 +33,7 @@ export class AuthService {
     this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
+        this.authStatusListener.next(true);
       });
   }
 }
--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor(){}
+export class HeaderComponent implements OnInit, OnDestroy {
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+
+
+  constructor(private authService: AuthService){}
+
+  ngOnInit(){
+    this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy(){
+    this.authListenerSubs.unsubscribe();
+  }
+
+
 }
